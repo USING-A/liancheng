9151d582765f047baaed21d785e55528e0426c7b
USING-A <166520556+USING-A@users.noreply.github.com>
2025-09-15 13:06:00 +0800

    Delete build directory

--- build/liancheng_socket/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liancheng/Desktop/robot/src/SimpleNetwork
-# Build directory: /home/liancheng/Desktop/robot/build/liancheng_socket
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
